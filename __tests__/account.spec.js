1220b03623c79bc20f0a718b21e187664ba95a97
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-04-24 14:01:11 +0100

    written second failing test

--- __tests__/account.spec.js
+++ __tests__/account.spec.js
@@ -1,7 +1,20 @@
 const Account = require('../src/account');
 
+beforeEach(() => {
+  account = new Account;
+})
+
 describe('Account class', () => {
+
   test('should create an object', () => {
     expect(new Account).toBeInstanceOf(Account);
   })
+
 });
+
+describe('Deposit', () => {
+  test('you can make a deposit', () => {
+    account.deposit(10)
+    expect(account.balance).toEqual(10)
+  })
+})
